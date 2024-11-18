450b2b0c1bd8d833d6f95a502ce8dfb810378e13
Nctdt <137032445@qq.com>
2024-11-18 09:20:16 +0800

    feat: switch theme (#4)

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,17 +1,29 @@
 'use client';
 
 import React from 'react';
+import { useTheme } from 'next-themes';
 
 import { SidebarTrigger } from '@/components/ui/sidebar';
 
 const Header = () => {
+  const { setTheme, theme } = useTheme();
+
+  const switchTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  };
+
   return (
-    <header className="w-full p-4 bg-white shadow-md flex justify-between items-center">
+    <header className="w-full p-4 shadow-md flex justify-between items-center">
       {/* 顶部导航栏 */}
       <nav className="flex space-x-4 items-center">
         <SidebarTrigger />
       </nav>
       <div className="flex space-x-4">
+        <button onClick={switchTheme}>切</button>
         <button>🔍</button>
         <button>🔔</button>
         <button>👤</button>
